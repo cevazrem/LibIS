2d9d40368c49a1b6071e11e311b543a80e3b9c81
Alex <71120846+cevazrem@users.noreply.github.com>
2022-01-14 18:54:19 +0300

    Add files via upload

--- client.java
+++ client.java
@@ -1,17 +1,45 @@
-import java.net.Socket; 
-import java.io.OutputStream;
-import java.io.DataOutputStream;
-public class WishesClient
-{
-   public static void main(String args[]) throws Exception
-   {
-     Socket sock = new Socket("127.0.0.1", 5000);       
-     String message1 = "Accept Best Wishes, Server";
-     OutputStream ostream = sock.getOutputStream();                 
-     DataOutputStream dos = new DataOutputStream(ostream);
-     dos.writeBytes(message1);                                                         
-     dos.close();                            
-     ostream.close();   
-     sock.close();
-  }
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private static Socket clientSocket; //сокет для общения
+    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
+    // мы узнаем что хочет сказать клиент?
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) {
+        try {
+            try {
+                // адрес - локальный хост, порт - 4004, такой же как у сервера
+                clientSocket = new Socket("localhost", 4004); // этой строкой мы запрашиваем
+                //  у сервера доступ на соединение
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                // читать соообщения с сервера
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // писать туда же
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                System.out.println("Listen:");
+                // если соединение произошло и потоки успешно созданы - мы можем
+                //  работать дальше и предложить клиенту что то ввести
+                // если нет - вылетит исключение
+                String word = reader.readLine(); // ждём пока клиент что-нибудь
+                // не напишет в консоль
+                out.write(word + "\n"); // отправляем сообщение на сервер
+                out.flush();
+                String serverWord = in.readLine(); // ждём, что скажет сервер
+                System.out.println(serverWord); // получив - выводим на экран
+            } finally { // в любом случае необходимо закрыть сокет и потоки
+                System.out.println("Client closed...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+    }
 }
--- server.java
+++ server.java
@@ -1,19 +1,47 @@
-import java.net.ServerSocket;            
-import java.net.Socket;            
-import java.io.InputStream;
-import java.io.DataInputStream;
-public class WishesServer
-{
-   public static void main(String args[]) throws Exception
-   {
-     ServerSocket sersock = new ServerSocket(5000); 
-     System.out.println("server is ready");  //  message to know the server is running
-     Socket sock = sersock.accept();               
-                                                                                          
-     InputStream istream = sock.getInputStream();  
-     DataInputStream dstream = new DataInputStream(istream);
-     String message2 = dstream.readLine();
-     System.out.println(message2);
-     dstream .close(); istream.close(); sock.close(); sersock.close();
-  }
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static Socket clientSocket; //сокет для общения
+    private static ServerSocket server; // серверсокет
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) {
+        try {
+            try  {
+                server = new ServerSocket(4004); // серверсокет прослушивает порт 4004
+                System.out.println("Server ready"); // хорошо бы серверу
+                //   объявить о своем запуске
+                clientSocket = server.accept(); // accept() будет ждать пока
+                //кто-нибудь не захочет подключиться
+                try { // установив связь и воссоздав сокет для общения с клиентом можно перейти
+                    // к созданию потоков ввода/вывода.
+                    // теперь мы можем принимать сообщения
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    // и отправлять
+                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                    String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                    System.out.println(word);
+                    // не долго думая отвечает клиенту
+                    out.write("Recieved message : " + word + "\n");
+                    out.flush(); // выталкиваем все из буфера
+
+                } finally { // в любом случае сокет будет закрыт
+                    clientSocket.close();
+                    // потоки тоже хорошо бы закрыть
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Server closed!");
+                    server.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
