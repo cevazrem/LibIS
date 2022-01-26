8a2fa4767463cc776c86dda6e0529f228dd0988d
Alex <71120846+cevazrem@users.noreply.github.com>
2022-01-26 11:50:17 +0300

    Add files via upload

--- server.java
+++ server.java
@@ -11,6 +11,8 @@ public class Server {
     private static BufferedReader in; // read pipe
     private static BufferedWriter out; // write pipe
 
+    //todo add function to create JSON
+
     public static void main(String[] args) {
         try {
             try  {
@@ -22,14 +24,36 @@ public class Server {
                     in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // in canal
                     out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); // out canal
                     String word = in.readLine();
-                    System.out.println(word);
-                    out.write(word + "\n");
-                    out.flush(); // pop buffer
-                    word = in.readLine();
                     while (!word.equals("stop") && word != null) {
+                        if (word.equals("DrawClient")) {
+                            //test
+                            out.write("{{\"id\":\"1\",\"fio\":\"test test test\",\"birth_date\":\"29/05/2001\",\"reg_date\":\"01/01/2022\",\"end_date\":\"\",\"phone\":\"89850550655\",\"ticket\":\"1\"},{\"id\":\"2\",\"fio\":\"test\",\"birth_date\":\"29/06/2001\",\"reg_date\":\"01/02/2022\",\"end_date\":\"\",\"phone\":\"89850550655\",\"ticket\":\"2\"}}\n");
+                            out.flush(); // pop buffer
+                            //test
+                            //todo create json db values
+                        } else if (word.equals("DrawTickets")) {
+                            //todo create json db values
+                            out.write("Recieved message" + word +"\n");
+                            out.flush(); // pop buffer
+                        } else if (word.equals("DrawReserves")) {
+                            //todo create json db values
+                            out.write("Recieved message" + word +"\n");
+                            out.flush(); // pop buffer
+                        } else if (word.equals("DrawBooks")) {
+                            //todo create json db values
+                            out.write("Recieved message" + word +"\n");
+                            out.flush(); // pop buffer
+                        } else if (word.equals("DrawAuthors")) {
+                            //todo create json db values
+                            out.write("Recieved message" + word +"\n");
+                            out.flush(); // pop buffer
+                        } else if (word.equals("DrawPublishers")) {
+                            //todo create json db values
+                            out.write("Recieved message" + word +"\n");
+                            out.flush(); // pop buffer
+                        }
+                        //todo another operations(delete/add/view)
                         System.out.println(word);
-                        out.write("Recieved message :" + word + "\n");
-                        out.flush(); // pop buffer
                         word = in.readLine();
                     }
                 } finally { // close socket
